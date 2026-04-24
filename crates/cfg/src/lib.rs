009f41fed923731a8b8fb6f2dec5ee93b9b51949
p-mikeg <mike.greenbaum@mail.huji.ac.il>
2026-04-25 00:01:34 +0300

    test(cfg): drop test-only lint suppression now that src has no inline tests

--- crates/cfg/src/lib.rs
+++ crates/cfg/src/lib.rs
@@ -1,13 +1,3 @@
-#![cfg_attr(
-    test,
-    allow(
-        clippy::panic,
-        clippy::unwrap_used,
-        clippy::expect_used,
-        clippy::unreachable
-    )
-)]
-
 //! Control-flow graph construction for the Strider binary analysis framework.
 //!
 //! This crate lifts a binary function to a [`Cfg`] of basic blocks using
